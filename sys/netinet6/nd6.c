1c66caf0d478201a59644b5fb00eba709e761110
Pouria Mousavizadeh Tehrani <pouria@FreeBSD.org>
2026-01-27 02:25:20 +0330

    ndp: Add support for Gratuitous Neighbor Discovery (GRAND)
    Implement RFC 4861 Section 7.2.6 and RFC 9131, which is also
    address one of the IPv6 deployment issues in RFC 9898 Section 3.9.
    GRAND should be triggered by a change in link-layer address of interface
    or by configuration of a new global ipv6 address AFTER DAD completes.
    
    Reviewed by: glebius
    Differential Revision: https://reviews.freebsd.org/D55015

--- sys/netinet6/nd6.c
+++ sys/netinet6/nd6.c
@@ -216,6 +216,24 @@ nd6_lle_event(void *arg __unused, struct llentry *lle, int evt)
 }
 
 /*
+ * Sends gratuitous NAs for each ifaddr to notify other
+ * nodes about the address change.
+ */
+static __noinline void
+nd6_handle_ifllchange(struct ifnet *ifp)
+{
+	struct ifaddr *ifa;
+	struct epoch_tracker et;
+
+	NET_EPOCH_ENTER(et);
+	CK_STAILQ_FOREACH(ifa, &ifp->if_addrhead, ifa_link) {
+		if (ifa->ifa_addr->sa_family == AF_INET6)
+			nd6_grand_start(ifa, ND6_QUEUE_FLAG_LLADDR);
+	}
+	NET_EPOCH_EXIT(et);
+}
+
+/*
  * A handler for interface link layer address change event.
  */
 static void
@@ -226,6 +244,9 @@ nd6_iflladdr(void *arg __unused, struct ifnet *ifp)
 		return;
 
 	lltable_update_ifaddr(LLTABLE6(ifp));
+
+	if ((ifp->if_flags & IFF_UP) != 0)
+		nd6_handle_ifllchange(ifp);
 }
 
 void
@@ -321,6 +342,8 @@ nd6_ifattach(struct ifnet *ifp)
 			/* If we globally accept rtadv, assume IPv6 on. */
 			nd->nd_flags &= ~ND6_IFF_IFDISABLED;
 	}
+
+	nd6_queue_init(nd);
 }
 
 void
@@ -333,6 +356,9 @@ nd6_ifdetach(struct ifnet *ifp)
 	CK_STAILQ_FOREACH_SAFE(ifa, &ifp->if_addrhead, ifa_link, next) {
 		if (ifa->ifa_addr->sa_family != AF_INET6)
 			continue;
+
+		/* make sure there are no queued ND6 */
+		nd6_queue_stop(ifa);
 
 		/* stop DAD processing */
 		nd6_dad_stop(ifa);
